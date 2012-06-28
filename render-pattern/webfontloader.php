0a96a92af0a5e80109758480d8cfe620979da69b
Jake Archibald <jaffathecake@gmail.com>
2012-06-28 15:08:50 +0100

    wtf?

--- render-pattern/webfontloader.php
+++ render-pattern/webfontloader.php
@@ -1,5 +1,5 @@
 <?php include '../includes/lib.php' ?><!DOCTYPE html>
-<html lang="en" class="ie-lte-8">
+<html lang="en">
 <head>
 	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
